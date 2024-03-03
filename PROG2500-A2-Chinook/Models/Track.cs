d5e0c75608c3340aeacd3592400000a6403899eb
Matthew MacKay <W0437676@campus.nscc.ca>
2024-03-03 17:04:16 -0400

    Added a partial class to add a $ to the beginning of the price tag

---
+++ PROG2500-A2-Chinook/Models/Track.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2500_A2_Chinook.Models
+{
+    public partial class Track
+    {
+        public string formattedPriceString
+        {
+            get { string price = this.UnitPrice.ToString();
+                string formattedString = '$' + price;
+                return formattedString;
+            }
+        }
+    }
+}
